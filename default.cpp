a1994e4d810a45bd2777cfd8ec4f3548d8fc4f25
ohhaeseong <43700074+ohsolution@users.noreply.github.com>
2020-10-08 15:01:44 +0900

    Create default.cpp

---
+++ default.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define ci(t) cin>>t
+#define co(t) cout<<t
+#define LL long long
+#define fa(i,a,b) for(int i=a;i<b;++i)
+#define fd(i,a,b) for(int i=a;i>b;--i)
+
+using namespace std;
+/* //if u need
+LL gcd(LL a, LL b) { return a % b ? gcd(b, a % b) : b; }
+LL lcm(LL a, LL b) { return (a * b) / gcd(a, b); }
+*/
+const int max_v = 1e5 + 7;
+const int INF = 1e9 + 7;
+const int mod = 1e9 + 7;
